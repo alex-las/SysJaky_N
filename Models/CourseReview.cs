8015f052e17a209c748ed8ff688ab60f6c8c1f20
alex-las <58659949+alex-las@users.noreply.github.com>
2025-09-09 18:19:02 +0200

    Add course reviews with admin moderation

---
+++ Models/CourseReview.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SysJaky_N.Models;
+
+public class CourseReview
+{
+    public int Id { get; set; }
+
+    [Required]
+    public int CourseId { get; set; }
+    public Course? Course { get; set; }
+
+    [Required]
+    public string UserId { get; set; } = string.Empty;
+    public ApplicationUser? User { get; set; }
+
+    [Required]
+    [Range(1, 5)]
+    public int Rating { get; set; }
+
+    [StringLength(1000)]
+    public string? Comment { get; set; }
+
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
