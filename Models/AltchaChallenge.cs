75f26b1f0aaf8385a70a02123c45a595e1856730
alex-las <58659949+alex-las@users.noreply.github.com>
2025-09-12 18:29:07 +0200

    Add salt to Altcha challenge

--- Models/AltchaChallenge.cs
+++ Models/AltchaChallenge.cs
@@ -4,5 +4,6 @@ public class AltchaChallenge
 {
     public string Id { get; set; } = string.Empty;
     public string Question { get; set; } = string.Empty;
+    public string Salt { get; set; } = string.Empty;
 }
 
