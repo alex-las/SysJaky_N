2942af497b4bb30770d23d91044357a29c7c80f2
alex-las <58659949+alex-las@users.noreply.github.com>
2025-09-30 20:30:36 +0200

    Simplify design-time DbContext factory

--- Data/ApplicationDbContextFactory.cs
+++ Data/ApplicationDbContextFactory.cs
@@ -1,8 +1,6 @@
 using System;
-using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 namespace SysJaky_N.Data;
@@ -11,22 +9,8 @@ public sealed class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Ap
 {
     public ApplicationDbContext CreateDbContext(string[] args)
     {
-        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
-        var basePath = Directory.GetCurrentDirectory();
-
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(basePath)
-            .AddJsonFile("appsettings.json", optional: false)
-            .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
-            .AddUserSecrets<Program>(optional: true)
-            .AddEnvironmentVariables()
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-        if (string.IsNullOrWhiteSpace(connectionString))
-        {
-            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-        }
+        var connectionString = Environment.GetEnvironmentVariable("MIGRATIONS_CONNSTR")
+                              ?? "Server=localhost;Database=SysJaky_N;User=root;Password=Kocour07!;";
 
         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
         optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)));
