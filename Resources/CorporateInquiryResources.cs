9d544532d194d6f0133fa925e2ea45590555e6b1
alex-las <58659949+alex-las@users.noreply.github.com>
2025-10-08 11:28:09 +0200

    Add troubleshooting guide for corporate inquiry localization

--- Resources/CorporateInquiryResources.cs
+++ Resources/CorporateInquiryResources.cs
@@ -16,6 +16,10 @@ public static class CorporateInquiryResources
 
     private static string GetString(string name) => ResourceManager.GetString(name) ?? string.Empty;
 
+    public static string Step0Title => GetString(nameof(Step0Title));
+    public static string Step0Description => GetString(nameof(Step0Description));
+    public static string Step1Title => GetString(nameof(Step1Title));
+    public static string Step1Description => GetString(nameof(Step1Description));
     public static string ServiceTypeLabel => GetString(nameof(ServiceTypeLabel));
     public static string TrainingTypesLabel => GetString(nameof(TrainingTypesLabel));
     public static string ParticipantCountLabel => GetString(nameof(ParticipantCountLabel));
@@ -30,4 +34,21 @@ public static class CorporateInquiryResources
     public static string ContactEmailLabel => GetString(nameof(ContactEmailLabel));
     public static string ContactPhoneLabel => GetString(nameof(ContactPhoneLabel));
     public static string PhoneInvalid => GetString(nameof(PhoneInvalid));
+
+    public static string GetServiceTypeLabel(string serviceTypeKey) =>
+        string.IsNullOrWhiteSpace(serviceTypeKey)
+            ? string.Empty
+            : GetString($"ServiceType_{serviceTypeKey}");
+
+    public static string GetTrainingTypeLabel(string trainingTypeKey) =>
+        string.IsNullOrWhiteSpace(trainingTypeKey)
+            ? string.Empty
+            : GetString(trainingTypeKey);
+
+    public static string PackageRecommendationsTitle => GetString(nameof(PackageRecommendationsTitle));
+
+    public static string GetPackageRecommendation(string packageKey) =>
+        string.IsNullOrWhiteSpace(packageKey)
+            ? string.Empty
+            : GetString(packageKey);
 }
