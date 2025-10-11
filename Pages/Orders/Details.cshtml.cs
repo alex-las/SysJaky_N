0a9637d7032a4a5f9c903ae1dd9f0125a2829c38
alex-las <58659949+alex-las@users.noreply.github.com>
2025-10-11 09:37:43 +0200

    Add localized resources for orders and attendance pages

--- Pages/Orders/Details.cshtml.cs
+++ Pages/Orders/Details.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
 using SysJaky_N.Data;
 using SysJaky_N.Services;
 
@@ -12,14 +13,20 @@ namespace SysJaky_N.Pages.Orders;
 [Authorize]
 public class DetailsModel : OrderDetailsPageModel
 {
+    private readonly IStringLocalizer<DetailsModel> _localizer;
+
     public DetailsModel(
         ApplicationDbContext context,
         IConfiguration configuration,
         PaymentService paymentService,
         IConverter converter,
         IRazorViewEngine viewEngine,
-        ITempDataProvider tempDataProvider)
+        ITempDataProvider tempDataProvider,
+        IStringLocalizer<DetailsModel> localizer)
         : base(context, configuration, paymentService, converter, viewEngine, tempDataProvider)
     {
+        _localizer = localizer;
     }
+
+    protected override string PaymentReturnPagePath => _localizer["PaymentReturnPagePath"];
 }
