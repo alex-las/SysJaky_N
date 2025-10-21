b693d7876067a832a8313576c2f4ece86d34d8c7
Vlastimil Pršal <vlastimil.prsal@outlook.cz>
2025-10-21 10:25:17 +0200

    OK

--- Migrations/20251020210047_AddNewsletterIssues.cs
+++ Migrations/20251020210047_AddNewsletterIssues.cs
@@ -12,10 +12,6 @@ namespace SysJaky_N.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "IX_coursecategory_translations_CategoryId",
-                table: "coursecategory_translations");
-
             migrationBuilder.UpdateData(
                 table: "coursecategory_translations",
                 keyColumn: "Locale",
@@ -224,11 +220,6 @@ namespace SysJaky_N.Migrations
                 oldMaxLength: 10)
                 .Annotation("MySql:CharSet", "utf8mb4")
                 .OldAnnotation("MySql:CharSet", "utf8mb4");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_coursecategory_translations_CategoryId",
-                table: "coursecategory_translations",
-                column: "CategoryId");
         }
     }
 }
